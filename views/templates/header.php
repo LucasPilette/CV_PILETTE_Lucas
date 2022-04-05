910893a24f4aa0b73a62442226bb816b218b5c63
Lucas Pilette <pilettelucas@gmail.com>
2022-04-05 18:40:37 +0200

    Initialisation

---
+++ views/templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PILETTE Lucas - Curriculum Vitae</title>
+    <link rel="stylesheet" href="/public/css/styleHome.css">
+</head>
+<body>
+    <header>
+        <div class="headerContent">
+            <div class="title">
+                <h1>Junior <span class="greenEffect">Web-Dev</span></h1>
+            </div>
+            <div class="navMenu">
+                <ul>
+                    <li>À propos</li>
+                    <li>Portfolio</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+        
+    </header>
+
+    
+</body>
+</html>
